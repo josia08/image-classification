80361457410766da408632a1286c7437206ce606
josia08 <fanevampifalianaravalison@gmail.com>
2026-09-24 11:48:16 +0300

    activation cors

--- api/main.py
+++ api/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from api.routes.search import router as search_router
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="AI Image Search API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount(
     "/images",
